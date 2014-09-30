36731292b5591a45966215139bd8652987fb1069
Zach Tirrell <zbtirrell@gmail.com>
2014-09-30 09:55:53 -0400

    compile screenshots of posts

---
+++ shoot-it-up.php
@@ -0,0 +1,38 @@
+<?php
+
+$db = new mysqli( '127.0.0.1', 'root', '', 'pro' );
+if ( $db->connect_errno > 0 )
+{
+	die( 'Unable to connect to database [' . $db->connect_error . ']' );
+}// end if
+
+$sql = "SELECT * FROM wp_go_layout_screenshot WHERE `timestamp` = '0000-00-00 00:00:00' AND strategy = 'control' ORDER BY hits DESC  LIMIT 1000";
+if ( ! $result = $db->query( $sql ) )
+{
+	die( 'There was an error running the query [' . $db->error . ']' );
+}// end if
+
+while ( $row = $result->fetch_assoc() )
+{
+	`webkit2png --ignore-ssl-check -F -W 1000 \
+--js='webkit2png.stop();( function() {function callback() {gigaom_layout_test.init(true);jQuery("#comments-list,#hidden-sidebar,#sidebar,.gigaom-layout-test-panel,gigaom-layout-test-panel-json,#add,#ade,#adf").remove();webkit2png.start();}var s = document.createElement( "script" );s.src = "https://localhost/layout-test.js";if ( s.addEventListener ) {s.addEventListener( "load", callback, false );} else if( s.readyState ) {s.onreadystatechange = callback;}document.body.appendChild( s );} )();' {$row['url']}`;
+
+	$sql = "UPDATE wp_go_layout_screenshot SET
+		`timestamp` = NOW()
+		WHERE url = ?
+		  AND strategy = 'control'
+		  AND `timestamp` = '0000-00-00 00:00:00'";
+	$stmt = $db->prepare( $sql );
+
+	$stmt->bind_param( 's',
+		$row['url']
+	);
+
+	$ok = $stmt->execute();
+
+	if ( ! $ok )
+	{
+		die( 'There was an error running the query [' . $db->error . ']' );
+	}//end if
+	$stmt->close();
+}//end while
